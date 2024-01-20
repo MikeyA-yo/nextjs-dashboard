c993785df10dded4cbb40feaffdd92e12b05af87
Ayomide Oluwatola <138298443+MikeyA-yo@users.noreply.github.com>
2024-01-20 16:25:38 +0100

    Update auth.config.ts

--- auth.config.ts
+++ auth.config.ts
@@ -7,13 +7,13 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
+      const isLoggedIn = !auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return NextResponse.redirect(new URL('/dashboard',nextUrl));
+        return NextResponse.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
     },
